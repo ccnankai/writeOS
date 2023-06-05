4cc7f0521cb8e8fc9904b0af1e4812351b83e662
ccnankai <swdailh@gmail.com>
2023-06-05 13:42:41 +0800

    finish print function

--- source/kernel/cpu/cpu.c
+++ source/kernel/cpu/cpu.c
@@ -1,5 +1,8 @@
-/**
-
+﻿/**
+ * CPU设置
+ *
+ * 作者：李述铜
+ * 联系邮箱: 527676163@qq.com
  */
 #include "comm/cpu_instr.h"
 #include "cpu/cpu.h"
@@ -13,33 +16,33 @@ static segment_desc_t gdt_table[GDT_TABLE_SIZE];
 void segment_desc_set(int selector, uint32_t base, uint32_t limit, uint16_t attr) {
     segment_desc_t * desc = gdt_table + (selector >> 3);
 
-    // 如果界限比较长，将长度单位换成4KB
-    if (limit > 0xfffff) {
-        attr |= 0x8000;
-        limit /= 0x1000;
-    }
-    desc->limit15_0 = limit & 0xffff;
-    desc->base15_0 = base & 0xffff;
-    desc->base23_16 = (base >> 16) & 0xff;
-    desc->attr = attr | (((limit >> 16) & 0xf) << 8);
-    desc->base31_24 = (base >> 24) & 0xff;
+	// 如果界限比较长，将长度单位换成4KB
+	if (limit > 0xfffff) {
+		attr |= 0x8000;
+		limit /= 0x1000;
+	}
+	desc->limit15_0 = limit & 0xffff;
+	desc->base15_0 = base & 0xffff;
+	desc->base23_16 = (base >> 16) & 0xff;
+	desc->attr = attr | (((limit >> 16) & 0xf) << 8);
+	desc->base31_24 = (base >> 24) & 0xff;
 }
 
 /**
  * 设置门描述符
  */
 void gate_desc_set(gate_desc_t * desc, uint16_t selector, uint32_t offset, uint16_t attr) {
-    desc->offset15_0 = offset & 0xffff;
-    desc->selector = selector;
-    desc->attr = attr;
-    desc->offset31_16 = (offset >> 16) & 0xffff;
+	desc->offset15_0 = offset & 0xffff;
+	desc->selector = selector;
+	desc->attr = attr;
+	desc->offset31_16 = (offset >> 16) & 0xffff;
 }
 
 /**
  * 初始化GDT
  */
 void init_gdt(void) {
-    // 全部清空
+	// 全部清空
     for (int i = 0; i < GDT_TABLE_SIZE; i++) {
         segment_desc_set(i << 3, 0, 0, 0);
     }
